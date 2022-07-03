7aa4c0a679007a65ba5d95909abce3528c0e55f9
raysantori <rayssacgomess@gmail.com>
2022-07-02 21:45:40 -0300

    refactor. css

--- desafio01-blog/arquivos/js/index.js
+++ desafio01-blog/arquivos/js/index.js
@@ -5,3 +5,9 @@ const $checkbox = document.querySelector('#switch')
 $checkbox.addEventListener('change', function(){
     $html.classList.toggle('dark-mode')
 })
+
+//rodapé
+var copyright = window.document.querySelector('#copyright')
+var dataAtual = new Date()
+var anoAtual = dataAtual.getFullYear()
+copyright.innerHTML = `&copy; ${anoAtual} raysantori`
