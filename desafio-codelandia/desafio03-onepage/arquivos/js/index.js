be37fba16cebdd7aafb201920a8e94023b8339f1
raysantori <rayssacgomess@gmail.com>
2022-07-14 15:02:25 -0300

    refactor. d01-blog

--- desafio-codelandia/desafio03-onepage/arquivos/js/index.js
+++
@@ -1,5 +0,0 @@
-//rodapé
-var copyright = window.document.querySelector('#copyright')
-var dataAtual = new Date()
-var anoAtual = dataAtual.getFullYear()
-copyright.innerHTML = `&copy; ${anoAtual} raysantori`
