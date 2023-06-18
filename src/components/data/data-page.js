5c71ae3c67868049a74b8d900c4a2230b4c22aa2
raysantori <contato@raysantori.com>
2023-06-18 13:51:40 -0300

    remove. projects 05-07-29

--- src/components/data/data-page.js
+++ src/components/data/data-page.js
@@ -18,37 +18,37 @@ const projectLiveCta = 'Live';
 const projectRepositoryCta = 'Code';
 const projectRepositoryLinks = 'https://github.com/raysantori/desafio-codelandia';
 var projectImages = [
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg', 
-    'ch022-codelandia/live-project.svg'
+    'ch022-codelandia/ch001-blog.png', 
+    'ch022-codelandia/ch002-jordanshoes.png', 
+    'ch022-codelandia/ch003-onepage.png', 
+    'ch022-codelandia/ch004-login.png', 
+    //'ch022-codelandia/live-project.svg', 
+    'ch022-codelandia/ch006-loki.png', 
+    //'ch022-codelandia/live-project.svg', 
+    'ch022-codelandia/ch009-portfolio.png', 
+    //'ch022-codelandia/live-project.svg'
 ];
 var projectTitles = [
     'Desafio 01: Blog', 
     'Desafio 02: JordanShoes', 
     'Desafio 03: OnePage', 
     'Desafio 04: Login', 
-    'Desafio 05: Studio Ghibli', 
+    //'Desafio 05: Studio Ghibli', 
     'Desafio 06: Loki', 
-    'Desafio 07: Valorant', 
+    //'Desafio 07: Valorant', 
     'Desafio 09: Portifólio', 
-    'Desafio 29: Anime'
+    //'Desafio 29: Anime'
 ];
 var projectLiveLinks = [
     'ch001-blog', 
     'ch002-jordanshoes', 
     'ch003-onepage', 
     'ch004-login', 
-    'ch005-studioghibli', 
+    //'ch005-studioghibli', 
     'ch006-loki', 
-    'ch007-valorant', 
+    //'ch007-valorant', 
     'ch009-portfolio', 
-    'ch029-anime'
+    //'ch029-anime'
 ];
 
 // content footer
